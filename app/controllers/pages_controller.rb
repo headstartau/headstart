ce69e4db78f075876dfc068c625c9353304a095f
Matt Milosavljevic <mr.mmilo@gmail.com>
2013-01-28 18:52:45 +1100

    more mocking up

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def index
+    render :layout => "one-col"
+  end
+
+  def event
+    render :layout => "two-col"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Headstart::Application.routes.draw do
-  get "logout"  => "sessions#destroy", :as => "logout"
-  get "login"   => "sessions#new", :as => "login"
+  get "logout"    => "sessions#destroy", :as => "logout"
+  get "login"     => "sessions#new", :as => "login"
   get "register"  => "users#new", :as => "register"
+  get "event"     => "pages#event", :as => "event"
 
   resources :users
   resources :sessions
