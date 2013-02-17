2009d012a3a2e260f28f109928ed39b0bd1dee2f
Vincent Brendel <vincent@VB.local>
2013-02-17 16:50:35 +1100

    put back user functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,20 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to :root, notice: 'User was successfully created.'
+    else
+      render action: "new"
+    end
+  end
+
   def edit
     @user = current_user
   end
