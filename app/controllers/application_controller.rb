27cebd5f0ff990a6543338b0f2ec23149e0461b3
Vincent Brendel <vincent@192-168-1-38.tpgi.com.au>
2013-02-18 12:27:12 +1100

    add bugherd, add RSVP button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  layout 'one-col'
+
   def not_authenticated
     redirect_to :login, :alert => "You need to be logged in to access that page."
   end
---
+++ config/initializers/bugherd.rb
@@ -0,0 +1,3 @@
+BugHerd.configure do |config|
+  config.project_key = '707eab4b-6981-4064-8c73-cde6554c1af7'
+end
